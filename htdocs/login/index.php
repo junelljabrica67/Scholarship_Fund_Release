7e5265a1bc22a73e0a97b9566a5a6d1bbe6ff304
junelljabrica67 <junell.jabrica67@gmail.com>
2025-10-09 13:02:04 +0800

    create login page

---
+++ htdocs/login/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+
+    <link rel="stylesheet" href="./styles.css">
+    <script type="module" src="./app.js"></script>
+</head>
+<body>
+    <form>
+        <h1>Log in to your account</h1>
+
+        <label>
+            Username: <br>
+            <input type="text" name="username">
+        </label>
+
+        <label>
+            Password: <br>
+            <input type="password" name="password">
+        </label>
+
+        <button type="submit">
+            Login
+        </button>
+    </form>
+</body>
+</html>
--- htdocs/signup/app.js
+++ htdocs/signup/app.js
@@ -10,13 +10,23 @@ document.addEventListener("DOMContentLoaded", () => {
  */
 async function signupHandler(e){
     e.preventDefault();
-
     const formData = new FormData( e.target );
 
     const response = await fetch("/api/register_user.php", {
         method: "POST",
         body: formData
     });
+
+    console.log(response.ok);
+
+    if (response.ok) {
+        alert("successful, now you can login");
+        window.location.href = '/login';
+    } else if (response.status === 400){
+        alert("An error occurred");
+    } else {
+        alert("Try again");
+    }
 
     console.log( await response.text() );
 }
